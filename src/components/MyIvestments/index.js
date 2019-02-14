ae64985bd003d1131b553857314c95cc5fc5364b
Vadym Bondarenko <danxil@list.ru>
2019-02-14 11:21:22 +0200

    Started cabinet

--- src/components/MyIvestments/index.js
+++ src/components/MyIvestments/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withLocalize } from 'react-localize-redux';
 import { compose, pure, lifecycle } from 'recompose';
@@ -9,6 +9,8 @@ import PageTitle from '../common/PageTitle';
 import withInvestments from '../../containers/withInvestments';
 
 const MyIvestments = ({ translate, investments }) => {
+  const completedInvestments = investments.filter(({ daysLeft }) => !daysLeft);
+  const activeInvestments = investments.filter(({ daysLeft }) => daysLeft);
   return (
     <div className={styles.myInvestments}>
       <Container>
@@ -16,7 +18,7 @@ const MyIvestments = ({ translate, investments }) => {
         <h3 className={styles.investmentsType}>{translate('ACTIVE_INVESTMENTS')}:</h3>
         <div className={styles.investments}>
           {
-            investments.filter(({ daysLeft }) => daysLeft).map((investment) => (
+            activeInvestments.map((investment) => (
               <Tariff
                 key={`investment-${investment.id}`}
                 tariffTitle={investment.tariff.name}
@@ -39,29 +41,35 @@ const MyIvestments = ({ translate, investments }) => {
             ))
           }
         </div>
-        <h3 className={styles.investmentsType}>{translate('COMPLETED_INVESTMENTS')}:</h3>
-        <div className={styles.investments}>
-          {
-            investments.filter(({ daysLeft }) => !daysLeft).map((investment) => (
-              <Tariff
-                key={`investment-${investment.id}`}
-                tariffTitle="Base"
-                amount="200$"
-                completed
-                lines={[
-                  {
-                    label: translate('DAILY_PROFIT'),
-                    value: `${investment.tariff.percentage} %`,
-                  },
-                  {
-                    label: translate('PROFIT_RECEIVED'),
-                    value: `${investment.tariff.duration * (investment.tariff.percentage * investment.amount)} $`
-                  },
-                ]}
-              />
-            ))
-          }
-        </div>
+        {
+          completedInvestments.length ? (
+            <Fragment>
+              <h3 className={styles.investmentsType}>{translate('COMPLETED_INVESTMENTS')}:</h3>
+              <div className={styles.investments}>
+                {
+                  completedInvestments.map((investment) => (
+                    <Tariff
+                      key={`investment-${investment.id}`}
+                      tariffTitle="Base"
+                      amount="200$"
+                      completed
+                      lines={[
+                        {
+                          label: translate('DAILY_PROFIT'),
+                          value: `${investment.tariff.percentage} %`,
+                        },
+                        {
+                          label: translate('PROFIT_RECEIVED'),
+                          value: `${investment.tariff.duration * (investment.tariff.percentage * investment.amount)} $`
+                        },
+                      ]}
+                    />
+                  ))
+                }
+              </div>
+            </Fragment>
+          ) : null
+        }
       </Container>
     </div>
   );
