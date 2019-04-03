01eb5ac2908cacecd1f4fb4367aadb3787c99ce7
Vadym Bondarenko <danxil@list.ru>
2019-04-04 02:23:45 +0300

    Added tele

--- src/redux/tariffs/actions.js
+++ src/redux/tariffs/actions.js
@@ -5,7 +5,9 @@ export const getTariffs = () => ({
     endpoint: `${process.env.REACT_APP_BASE_REST_URL}/tariffs`,
     credentials: 'include',
     method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     types: [
       'GET_TARIFFS_REQUEST',
       { type: 'GET_TARIFFS_SUCCESS' },
